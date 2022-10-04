5205506ebd2eb84e2641784aaedb51d599a64aa0
samro01 <srozario22@gmail.com>
2022-10-04 11:39:47 +0200

    rechart added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import './App.css';
+import Assignments from './components/Assignments/Assignments';
 import NavBar from './components/NavBar/NavBar';
+import PhoneBar from './components/PhoneBar/PhoneBar';
 import Pricing from './components/Pricing/Pricing';
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <h1 className="text-3xl font-bold underline">This is tailwind</h1>
       <p>this is also same.</p>
       <Pricing></Pricing>
+      <Assignments></Assignments>
+      <PhoneBar></PhoneBar>
     </div>
   );
 }
